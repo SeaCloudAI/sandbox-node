e6470b7dc8cf76fc0c03eb40006790a8bd3b35d3
小薇薇 <xiaoweiwei@haiyi.art>
2026-05-20 11:18:38 +0800

    refactor: expose template workdir field

--- src/build/types.ts
+++ src/build/types.ts
@@ -182,7 +182,7 @@ export interface TemplateResponse {
   ttlSeconds?: number;
   daemonImage?: string;
   cloudsinkURL?: string;
-  sandboxRoot?: string;
+  workdir?: string;
   probeTargets?: string;
   probeIntervalSecs?: number;
   probeTimeoutSecs?: number;
